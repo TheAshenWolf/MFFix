d13434064a27e6488a5311390693a3175b6f5ab1
TheAshenWolf <jirirkas.skola@gmail.com>
2023-08-12 17:03:41 +0200

    added gitignore

--- obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MFFix
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "MFFix";
-        public const string PLUGIN_NAME = "MFFix";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
